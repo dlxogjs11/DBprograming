2dff4236a22faf14e1f2247ad377f3a24d2fe314
sorrylim <52535550+sorrylim@users.noreply.github.com>
2020-10-11 15:52:06 +0900

    Merge테스트
    테스트

--- MarketBrowser/MarketBrowser/Program.cs
+++ MarketBrowser/MarketBrowser/Program.cs
@@ -14,8 +14,8 @@ namespace MarketBrowser
         [STAThread]
         static void Main()
         {
+            Application.SetCompatibleTextRenderingDefault(true);
             Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
         }
     }
